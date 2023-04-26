bea899766b176c355ec9f48f331b4355cdef3474
gfg7 <nakonechnaya040702@gmail.com>
2023-04-26 08:46:22 +0000

    set caching indexing
    contract
    
    set caching indexing

--- Contract/Cache/ICacheProvider.cs
+++ Contract/Cache/ICacheProvider.cs
@@ -2,6 +2,7 @@ namespace TestTask.Contract.Cache;
 
 public interface ICacheProvider<T> where T: notnull
 {
-    Task Upsert(string key, T obj, TimeSpan? time=null);
-    Task Get(string key, IQueryable<T> filter = null);
+    IEnumerable<T> GetAll(Func<T, bool> filter = null);
+    Task<T?> GetByKey(string key);
+    Task Insert(T obj, TimeSpan time);
 }
---
+++ Contract/Route.cs
@@ -0,0 +1,42 @@
+using System.Reflection.Metadata;
+using Redis.OM.Modeling;
+
+namespace TestTask;
+
+[Document(StorageType = StorageType.Json)]
+public class Route
+{
+    // Mandatory
+    // Identifier of the whole route
+    [RedisIdField]
+    public Guid Id { get; set; }
+    
+    // Mandatory
+    // Start point of route
+    [Indexed]
+    public string Origin { get; set; }
+    
+    // Mandatory
+    // End point of route
+    [Indexed]
+    public string Destination { get; set; }
+    
+    // Mandatory
+    // Start date of route
+    [Indexed]
+    public DateTime OriginDateTime { get; set; }
+    
+    // Mandatory
+    // End date of route
+    [Indexed]
+    public DateTime DestinationDateTime { get; set; }
+    
+    // Mandatory
+    // Price of route
+    [Indexed]
+    public decimal Price { get; set; }
+    
+    // Mandatory
+    // Timelimit. After it expires, route became not actual
+    public DateTime TimeLimit { get; set; }
+}
