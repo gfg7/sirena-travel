d812bf42702a20ed2b8a8f2f63749bd9ff65b883
gfg7 <nakonechnaya040702@gmail.com>
2023-04-26 08:53:20 +0000

    add logging

--- Services/Providers/ProviderTwoService.cs
+++ Services/Providers/ProviderTwoService.cs
@@ -1,3 +1,4 @@
+using sirena_travel.Services;
 using TestTask.Contract;
 using TestTask.Contract.Providers;
 using TestTask.Services.Mappers;
@@ -7,18 +8,33 @@ namespace TestTask.Services.Providers;
 
 public class ProviderTwoService : BaseWebProvider, ISearchProvider
 {
+    public ProviderTwoService(ILogger<ProviderTwoService> logger) : base(logger)
+    {
+    }
+
     public async Task<SearchResponse> GetRoute(SearchRequest request)
     {
         var @args = request.MapProviderTwoRequest();
-        var response = await _client.PostAsJsonAsync("http://provider-two/api/v1/search", args);
+        _logger.LogInformation($"Get route from {_client.BaseAddress}");
+        var response = await _client.PostAsJsonAsync(EnvironmentUtil.Get("ProviderTwoService"), args);
         var result = await response.Content.ReadFromJsonAsync<ProviderTwoSearchResponse>();
+
 
         return result!.Routes.MapResponse();
     }
 
     public override async Task Ping()
     {
-        var result = await _client.GetAsync("http://provider-two/api/v1/ping");
+        var result = await _client.GetAsync(EnvironmentUtil.Get("ProviderTwoPing"));
+        _logger.LogInformation($"Ping {_client.BaseAddress} {result.RequestMessage.RequestUri} {result.StatusCode}");
         result.EnsureSuccessStatusCode();
+    }
+
+    protected override HttpClient SetupClient()
+    {
+        return new HttpClient()
+        {
+            BaseAddress = new Uri(EnvironmentUtil.Get("ProviderTwo"))
+        };
     }
 }
